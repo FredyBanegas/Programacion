2febdac37f555329b9a06fe862c8f05474819418
matebook-14 <matebook-14@LAPTOP-GGI45GUH>
2026-07-24 21:11:36 -0600

    Mostrar en pantalla usando los prints y captura de datos al declarar al declarar variables

---
+++ Hola_Mundo2/src/hola_mundo2/Hola_Mundo2.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hola_mundo2;
+
+/**
+ *
+ * @author matebook-14
+ */
+public class Hola_Mundo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  
+    System.out.println("Hola. . me llamo Fredy");
+    System.out.println("Este sera mi 1er");
+    System.out.println("PROGRAMAA!!!!!!");
+    
+    System.out.print("Vamos camino a");
+    System.out.print(" la playa de ");
+    System.out.print("Roatan!!");
+    
+
+    System.out.println("");
+    
+    System.out.printf("Tu apellido es %s","Banegas");
+    System.out.println("");
+    
+    System.out.printf("%s apellido es","Banegas");
+    System.out.println("");
+    
+    System.out.printf("Tu apellido %S es","Banegas");
+    System.out.println("");
+    
+    System.out.println("Hola \n este es \n un nuevo \n amanecer");
+    System.out.println("Hola \t este es \t un nuevo \t amanecer");
+
+   //Variables
+    int edad;       //Tipo Entero
+    double peso;    //Tipo Decimal
+    char inicial;   //Tipo Caracter ->letras, digitos,simbolo especial
+    String nombre;  //Tipo Cadena
+    boolean estado; //Tipo Estado ->true(verdadero) false(falso)
+    
+    edad = 29;
+    peso = 55.4;
+    inicial = 'B';
+    nombre = "Basilio";
+    
+    System.out.printf("Tengo %d anios \n",edad);
+    System.out.printf("Mi peso ideal es %.2f \n",peso);
+    System.out.printf("Dime %c por mi inicial",inicial);
+    System.out.printf("%s es mi nombre",nombre);
+    
+}//Fin de Main
+}
